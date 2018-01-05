3145660101611e788c2388816fa06f9974a8b00a
Maxi Rosson <maxirosson@gmail.com>
2018-01-05 20:17:11 -0300

    Added android support

---
+++ android_app/src/main/java/sample/android_sample/SampleActivity.java
@@ -0,0 +1,6 @@
+package sample.android_sample;
+
+import android.app.Activity;
+
+public class SampleActivity extends Activity {
+}
---
+++ android_app/src/main/java/sample/android_sample/SampleApplication.java
@@ -0,0 +1,14 @@
+package sample.android_sample;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+
+public class SampleApplication extends Application {
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
