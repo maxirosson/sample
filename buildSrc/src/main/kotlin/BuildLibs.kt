0502c2b41e590f6e44b2bcf0f40c6deba1bf4223
Maxi Rosson <maxirosson@gmail.com>
2019-09-13 13:31:11 -0300

    asdasdsa

--- buildSrc/src/main/kotlin/BuildLibs.kt
+++ buildSrc/src/main/kotlin/BuildLibs.kt
@@ -1,5 +1,6 @@
 object BuildLibs {
     const val jdroid_component_builder = "com.jdroidtools:jdroid-component-builder:2.1.0"
+    const val jdroid_java = "com.jdroidtools:jdroid-java-core:1.0.0"
 
 
     const val android = "com.android.tools.build:gradle:3.4.1"
