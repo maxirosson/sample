4d2c4e6d674b95814064905624eae98d0a946911
Maxi Rosson <maxirosson@gmail.com>
2019-09-13 23:06:38 -0300

    Upgraded dependencies

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,3 +1,3 @@
 object Libs {
-    const val KOTLIN = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.40"
+    const val KOTLIN = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.50"
     const val ANDROIDX_ROOM_RUNTIME = "androidx.room:room-runtime:2.1.0"}
