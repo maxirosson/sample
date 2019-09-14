a513c335a7f97cc9810255937dcf08fda928d5da
Maxi Rosson <maxirosson@gmail.com>
2019-09-13 23:01:23 -0300

    sdasd

--- buildSrc/src/main/kotlin/BuildLibs.kt
+++ buildSrc/src/main/kotlin/BuildLibs.kt
@@ -1,7 +1,4 @@
 object BuildLibs {
-    const val jdroid_component_builder = "com.jdroidtools:jdroid-component-builder:2.1.0"
-    const val jdroid_java = "com.jdroidtools:jdroid-java-core:1.0.0"
-
-
-    const val android = "com.android.tools.build:gradle:3.4.1"
+    const val KOTLIN_GRADLE_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.40"
+    const val ANDROID_GRADLE_PLUGIN = "com.android.tools.build:gradle:3.4.0"
 }
--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,3 +1,3 @@
 object Libs {
-    const val koin = "org.koin:koin-core:2.0.0"
-}
+    const val KOTLIN = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.40"
+    const val ANDROIDX_ROOM_RUNTIME = "androidx.room:room-runtime:2.1.0"}
