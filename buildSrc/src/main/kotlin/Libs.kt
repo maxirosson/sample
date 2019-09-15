ee703d64b7369093b803dd79e6069ce11d99c3b5
jdroid-ci <jdroidtools@gmail.com>
2019-09-14 21:55:54 -0300

    Upgraded dependencies

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,4 +1,4 @@
 object Libs {
-    const val KOTLIN = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.40"
+    const val KOTLIN = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.50"
     const val ANDROIDX_ROOM_RUNTIME = "androidx.room:room-runtime:2.1.0"
 }
