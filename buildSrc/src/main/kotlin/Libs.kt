17d89b756574d80becff74ac95c0c6dc7637b53a
Maxi Rosson <maxirosson@gmail.com>
2019-09-14 21:07:32 -0300

    asdas

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,3 +1,4 @@
 object Libs {
     const val KOTLIN = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.40"
-    const val ANDROIDX_ROOM_RUNTIME = "androidx.room:room-runtime:2.1.0"}
+    const val ANDROIDX_ROOM_RUNTIME = "androidx.room:room-runtime:2.1.0"
+}
