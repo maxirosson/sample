c2b5d48233d6764cea7d4fa82f42537c377fdf94
jdroid-ci <jdroidtools@gmail.com>
2019-09-22 22:59:50 -0300

    asdads

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,4 +1,12 @@
 object Libs {
     const val KOTLIN = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.40"
     const val ANDROIDX_ROOM_RUNTIME = "androidx.room:room-runtime:2.1.0"
+    const val FIREBASE_CORE = "com.google.firebase:firebase-core:1.0.0"
+    const val FIREBASE_ANALYTICS = "com.google.firebase:firebase-analytics:1.0.0"
+    const val FIREBASE_MESSAGING = "com.google.firebase:firebase-messaging:17.0.0"
+    const val FIREBASE_PERF = "com.google.firebase:firebase-perf:1.0.0"
+    const val FIREBASE_JOB_DISPATCHER = "com.firebase:firebase-jobdispatcher:0.1.0"
+    const val INVALID_DEPENDENCY = "aaa:bbb:1.0.0"
+    const val EXCLUDED_DEPENDENCY_1 = "ccc:bbb:1.0.0"
+    const val EXCLUDED_DEPENDENCY_2 = "ddd:bbb:1.0.0"
 }
