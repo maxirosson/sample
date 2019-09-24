069d7f7934f15b5cad2d53d0e2baa7f52a1bd29d
jdroid-ci <jdroidtools@gmail.com>
2019-09-23 21:03:28 -0300

    Upgraded com.google.firebase:firebase-messaging from 17.0.0 to 20.0.0

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -3,7 +3,7 @@ object Libs {
     const val ANDROIDX_ROOM_RUNTIME = "androidx.room:room-runtime:2.1.0"
     const val FIREBASE_CORE = "com.google.firebase:firebase-core:17.2.0"
     const val FIREBASE_ANALYTICS = "com.google.firebase:firebase-analytics:17.2.0"
-    const val FIREBASE_MESSAGING = "com.google.firebase:firebase-messaging:17.0.0"
+    const val FIREBASE_MESSAGING = "com.google.firebase:firebase-messaging:20.0.0"
     const val FIREBASE_PERF = "com.google.firebase:firebase-perf:1.0.0"
     const val FIREBASE_JOB_DISPATCHER = "com.firebase:firebase-jobdispatcher:0.8.6"
     const val INVALID_DEPENDENCY = "aaa:bbb:1.0.0"
