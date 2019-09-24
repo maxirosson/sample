ecf52eca15f2836b022c53a5b3c9411f2b275b27
jdroid-ci <jdroidtools@gmail.com>
2019-09-23 21:57:34 -0300

    Upgraded com.google.firebase:firebase-core from 1.0.0 to 17.2.0

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,8 +1,8 @@
 object Libs {
     const val KOTLIN = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.40"
     const val ANDROIDX_ROOM_RUNTIME = "androidx.room:room-runtime:2.0.0"
-    const val FIREBASE_CORE = "com.google.firebase:firebase-core:1.0.0"
-    const val FIREBASE_ANALYTICS = "com.google.firebase:firebase-analytics:17.2.0"
+    const val FIREBASE_CORE = "com.google.firebase:firebase-core:17.2.0"
+    const val FIREBASE_ANALYTICS = "com.google.firebase:firebase-analytics:1.0.0"
     const val FIREBASE_MESSAGING = "com.google.firebase:firebase-messaging:17.0.0"
     const val FIREBASE_PERF = "com.google.firebase:firebase-perf:1.0.0"
     const val FIREBASE_JOB_DISPATCHER = "com.firebase:firebase-jobdispatcher:0.1.0"
