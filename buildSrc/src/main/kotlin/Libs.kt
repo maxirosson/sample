37f16ce7e18346f3ccabb3164c23ef0e7454b7b3
jdroid-ci <jdroidtools@gmail.com>
2019-10-14 16:31:36 -0300

    Upgraded com.firebase:firebase-jobdispatcher from 0.1.0 to 0.8.6

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -6,7 +6,7 @@ object Libs {
     const val FIREBASE_ANALYTICS = "com.google.firebase:firebase-analytics:1.0.0"
     const val FIREBASE_MESSAGING = "com.google.firebase:firebase-messaging:17.0.0"
     const val FIREBASE_PERF = "com.google.firebase:firebase-perf:1.0.0"
-    const val FIREBASE_JOB_DISPATCHER = "com.firebase:firebase-jobdispatcher:0.1.0"
+    const val FIREBASE_JOB_DISPATCHER = "com.firebase:firebase-jobdispatcher:0.8.6"
     const val INVALID_DEPENDENCY = "aaa:bbb:1.0.0"
     const val EXCLUDED_DEPENDENCY_1 = "ccc:bbb:1.0.0"
     const val EXCLUDED_DEPENDENCY_2 = "ddd:bbb:1.0.0"
