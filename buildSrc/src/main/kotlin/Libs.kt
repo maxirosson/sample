0d8f10738321ca4f980b7c88c4d44c8d7d9e0642
jdroid-ci <jdroidtools@gmail.com>
2019-10-14 16:31:48 -0300

    Upgraded com.google.firebase:firebase-perf from 1.0.0 to 19.0.0

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -5,7 +5,7 @@ object Libs {
     const val FIREBASE_CORE = "com.google.firebase:firebase-core:17.2.0"
     const val FIREBASE_ANALYTICS = "com.google.firebase:firebase-analytics:17.2.0"
     const val FIREBASE_MESSAGING = "com.google.firebase:firebase-messaging:20.0.0"
-    const val FIREBASE_PERF = "com.google.firebase:firebase-perf:1.0.0"
+    const val FIREBASE_PERF = "com.google.firebase:firebase-perf:19.0.0"
     const val FIREBASE_JOB_DISPATCHER = "com.firebase:firebase-jobdispatcher:0.1.0"
     const val INVALID_DEPENDENCY = "aaa:bbb:1.0.0"
     const val EXCLUDED_DEPENDENCY_1 = "ccc:bbb:1.0.0"
