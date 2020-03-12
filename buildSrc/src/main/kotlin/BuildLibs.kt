8db1181a6425e12c381681d83f765dcd33f803d7
jdroid-ci <jdroidtools@gmail.com>
2020-03-12 19:40:23 -0300

    a

--- buildSrc/src/main/kotlin/BuildLibs.kt
+++ buildSrc/src/main/kotlin/BuildLibs.kt
@@ -1,4 +1,5 @@
 object BuildLibs {
-    const val KOTLIN_GRADLE_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.40"
-    const val ANDROID_GRADLE_PLUGIN = "com.android.tools.build:gradle:3.4.0"
+    const val KOTLIN_GRADLE_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.60"
+    const val ANDROID_GRADLE_PLUGIN = "com.android.tools.build:gradle:3.6.1"
+    const val RELEASES_HUB_GRADLE_PLUGIN = "com.releaseshub:releases-hub-gradle-plugin:1.4.0-SNAPSHOT"
 }
