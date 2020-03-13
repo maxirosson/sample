30863f2abec1b938659ff9efff5edc60aa2302c1
jdroid-ci <jdroidtools@gmail.com>
2020-03-13 09:04:21 -0300

    asds

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -2,7 +2,7 @@ object Libs {
     const val KOTLIN = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.60"
 //    const val ANDROIDX_ROOM_RUNTIME_2 = "androidx.room:room-runtime:2.0.0"
 //    const val ANDROIDX_ROOM_RUNTIME_3 = "androidx.room:room-ktx:2.0.0"
-      const val FIREBASE_CRASH = "androix.work:work-runtime:2.3.0"
+      const val FIREBASE_CRASH = "androidx.work:work-runtime:2.3.0"
 //    const val FIREBASE_ANALYTICS = "com.google.firebase:firebase-analytics:1.0.0"
 //    const val FIREBASE_MESSAGING = "com.google.firebase:firebase-messaging:17.0.0"
 //    const val FIREBASE_PERF = "com.google.firebase:firebase-perf:1.0.0"
