a03281b45a395e1dc642d6eeacb39622bdedd599
jdroid-ci <jdroidtools@gmail.com>
2021-10-30 17:58:18 +0000

    Upgraded com.google.firebase:firebase-messaging from 17.0.0 to 23.0.0

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -2,6 +2,6 @@ object Libs {
     const val KOTLIN = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.60"
     const val FIREBASE_CRASH = "androidx.work:work-runtime:2.3.0"
     const val FIREBASE_ANALYTICS = "com.google.firebase:firebase-analytics:20.0.0"
-    const val FIREBASE_MESSAGING = "com.google.firebase:firebase-messaging:17.0.0"
+    const val FIREBASE_MESSAGING = "com.google.firebase:firebase-messaging:23.0.0"
     const val FIREBASE_PERF = "com.google.firebase:firebase-perf:20.0.3"
 }
