8a30ae19831be5b070d8e8e564229cb97ad6377d
jdroid-ci <jdroidtools@gmail.com>
2021-10-30 17:58:05 +0000

    Upgraded org.jetbrains.kotlin:kotlin-stdlib-jdk8 from 1.3.60 to 1.5.31

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,5 +1,5 @@
 object Libs {
-    const val KOTLIN = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.60"
+    const val KOTLIN = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.31"
     const val FIREBASE_CRASH = "androidx.work:work-runtime:2.3.0"
     const val FIREBASE_ANALYTICS = "com.google.firebase:firebase-analytics:1.0.0"
     const val FIREBASE_MESSAGING = "com.google.firebase:firebase-messaging:17.0.0"
