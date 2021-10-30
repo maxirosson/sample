2c5c7ecc3aa9cc3b14b5b696451734ef8f155d28
jdroid-ci <jdroidtools@gmail.com>
2021-10-30 17:58:31 +0000

    Upgraded com.android.tools.build:gradle from 3.6.2 to 7.0.3

--- buildSrc/src/main/kotlin/BuildLibs.kt
+++ buildSrc/src/main/kotlin/BuildLibs.kt
@@ -1,5 +1,5 @@
 object BuildLibs {
     const val KOTLIN_GRADLE_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.60"
-    const val ANDROID_GRADLE_PLUGIN = "com.android.tools.build:gradle:3.6.1"
+    const val ANDROID_GRADLE_PLUGIN = "com.android.tools.build:gradle:7.0.3"
     const val RELEASES_HUB_GRADLE_PLUGIN = "com.dipien:releases-hub-gradle-plugin:3.1.0"
 }
