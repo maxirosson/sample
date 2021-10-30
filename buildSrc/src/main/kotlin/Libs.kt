cfcbe2d499ef89c601e7ded08a03a6886668cba5
jdroid-ci <jdroidtools@gmail.com>
2021-10-30 17:58:18 +0000

    Upgraded com.google.firebase:firebase-perf from 1.0.0 to 20.0.3

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -3,5 +3,5 @@ object Libs {
     const val FIREBASE_CRASH = "androidx.work:work-runtime:2.3.0"
     const val FIREBASE_ANALYTICS = "com.google.firebase:firebase-analytics:1.0.0"
     const val FIREBASE_MESSAGING = "com.google.firebase:firebase-messaging:17.0.0"
-    const val FIREBASE_PERF = "com.google.firebase:firebase-perf:1.0.0"
+    const val FIREBASE_PERF = "com.google.firebase:firebase-perf:20.0.3"
 }
