3196fd4a4ec237e3aa8d98985e3fd05c51123bfd
jdroid-ci <jdroidtools@gmail.com>
2022-10-24 12:05:31 +0000

    "Upgraded org.jetbrains.kotlin:kotlin-gradle-plugin from 1.3.60 to 1.7.20"

--- buildSrc/src/main/kotlin/BuildLibs.kt
+++ buildSrc/src/main/kotlin/BuildLibs.kt
@@ -1,5 +1,5 @@
 object BuildLibs {
-    const val KOTLIN_GRADLE_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.60"
+    const val KOTLIN_GRADLE_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20"
     const val ANDROID_GRADLE_PLUGIN = "com.android.tools.build:gradle:3.6.1"
     const val RELEASES_HUB_GRADLE_PLUGIN = "com.dipien:releases-hub-gradle-plugin:3.1.1-SNAPSHOT"
 }
