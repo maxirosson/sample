821dea46c8b8bf129f6c837bec08fa63f1eddf7d
Maxi Rosson <maxirosson@gmail.com>
2023-12-28 02:21:38 +0100

    Update bitkey.py

--- .github/workflows/bitkey.py
+++ .github/workflows/bitkey.py
@@ -1,16 +1,26 @@
 # import libraries
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
-import pandas as pd
+from webdriver_manager.core.utils import ChromeType
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+
+chrome_options = Options()
+options = [
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage"
+]
+for option in options:
+    chrome_options.add_argument(option)
+
+driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
 # Define the URL
 url = "https://bitkey.world/en-US/products/bitkey"
